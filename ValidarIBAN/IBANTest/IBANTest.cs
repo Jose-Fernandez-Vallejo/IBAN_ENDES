90c6b3ce291759c763b4b62cc09b34eb463669a7
Jose Antonio Fernandez Vallejo <josejafv1997@gmail.com>
2019-05-14 20:13:20 +0200

    añado el proyecto del IBAN

---
+++ ValidarIBAN/IBANTest/IBANTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ValidarIBAN;
+
+namespace IBANTest
+{
+    [TestFixture]
+    public class IBANTest
+    {
+
+        [Test]
+        public void ElDigitoDeControlSeCalculaCorrectamente()
+        {
+            int acumulador1 = 5;    //debe devolver 11 - el numero insertado (6)
+            int acumulador2 = 10;   // debe devolver 1
+            int acumulador3 = 11;   // debe devolver 0
+
+            Assert.AreEqual("6", IBAN.CalculoAculmulador(acumulador1));
+            Assert.AreEqual("1", IBAN.CalculoAculmulador(acumulador2));
+            Assert.AreEqual("0", IBAN.CalculoAculmulador(acumulador3));
+        }
+
+        [Test]
+        public void ElCCEsValido()
+        {
+            string CC = "20852066623456789011";
+            Assert.IsTrue(IBAN.ValidarCC(CC));
+        }
+
+        [Test]
+        public void ElCCNoTieneLaLongitudCorrecta()
+        {
+            try
+            {
+                string CC = "123458001234567890";
+                IBAN.ValidarCC(CC);
+                Assert.Fail("Longitud Incorrecta");
+            }
+            catch (LongitudIncorrectaException)
+            { 
+                //algo
+            }
+        }
+
+        [Test]
+        public void ElCCTieneUnformatoIncorrecto()
+        {
+            try
+            {
+                string CC =  "12345j6789001234h56h";
+                IBAN.ValidarCC(CC);
+                Assert.Fail("Formato mal");
+            }
+            catch (ParametroFormatoIncorrecto)
+            {
+                //algo
+            }
+        }
+
+        [Test]
+        public void ElIBANSeCalculaCorrectamente()
+        { 
+            string CC =  "20852066623456789011";
+
+            Assert.AreEqual("17", IBAN.CalcularIBAN(CC));
+        }
+
+        [Test]
+        public void AlPasarleTodoJuntoCalculaCorrectamenteTantoIBANComoCC()
+        {
+            string CC = "ES1720852066623456789011";
+
+            Assert.IsTrue(IBAN.Validar(CC));
+        }
+
+        [Test]
+        public void DevuelveFalsoConUnNumeroDeControlInCorrectoEnELIBAN()
+        {
+            string CC = "ES1220852066623456789011";
+
+            Assert.IsFalse(IBAN.Validar(CC));
+        }
+
+
+        [Test]
+        public void DevuelveFalsoConUnNumeroDeControlInCorrectoEnELCC()
+        {
+            string CC = "ES1720852066223456789011";
+
+            Assert.IsFalse(IBAN.Validar(CC));
+        }
+    }
+}
---
+++ ValidarIBAN/ValidarIBAN/IBAN.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidarIBAN
+{
+
+    public class LongitudIncorrectaException : Exception { }
+    public class ParametroFormatoIncorrecto : Exception { }
+
+    //Ahora mismo solo funciona con EL IBAN "ES" porque no se exactamente que tipo de parametros y como se le van a pasar
+    public class IBAN
+    {
+        
+        public static bool ValidarCC(string Cuenta)
+        {
+            return (CalcularNumeroControl(Cuenta) == Cuenta.Substring(8, 2));
+        }
+
+        public static string CalcularNumeroControl(string cadena)
+        {
+            int[] multiplicador = new int[] { 4, 8, 5, 10, 9, 7, 3, 6, 0, 0, 1, 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+            string resultado;
+
+            int acumulador1 = 0;
+            int acumulador2 = 0;
+
+
+            if (cadena.Length != 20)
+            {
+                throw new LongitudIncorrectaException();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    acumulador1 += int.Parse(cadena[i].ToString()) * multiplicador[i];
+
+                    acumulador2 += int.Parse(cadena[i + 10].ToString()) * multiplicador[i + 10];
+                }
+                catch (FormatException)
+                {
+                    throw new ParametroFormatoIncorrecto();
+                }
+            }
+
+            resultado = CalculoAculmulador(acumulador1);
+            resultado += CalculoAculmulador(acumulador2);
+
+
+            return resultado;
+        }
+
+        public static string CalculoAculmulador(int acumulador)
+        {
+            int resultadoTmp = 11 - (acumulador % 11);
+            if (resultadoTmp == 10)
+                return "1";
+            else if (resultadoTmp == 11)
+                return "0";
+            else
+                return resultadoTmp.ToString();
+        }
+
+
+        public static string CalcularIBAN(string cadena)
+        {
+            string ES = "142800";
+            Decimal IBANCompleto = 0;
+            cadena = cadena + ES;
+            IBANCompleto = Decimal.Parse(cadena);
+            return (98 - (IBANCompleto % 97)).ToString();
+        }
+
+        public static bool Validar(string Cadena)
+        {
+            string CC = Cadena.Substring(4, 20);
+            string ControlIBAN = Cadena.Substring(2, 2);
+
+            if (!ValidarCC(CC))
+            {
+                return false;
+            }
+
+            return ControlIBAN == CalcularIBAN(CC);
+        }
+    }
+}
